f551c7c7666e3e31b0c6df7e1422baf410e541e8
jiash1998 <1459538273@qq.com>
2020-01-15 17:32:45 +0800

    20.01.15MapModify

--- src/views/Contral/son1Manage.vue
+++ src/views/Contral/son1Manage.vue
@@ -1,6 +1,34 @@
 <template>
   <div id="son1Manage">
-    <p>test</p>
+    <el-button type="success" plain><i class="el-icon-refresh"></i>刷新</el-button>
+    <!-- 分开写，从数据库读 -->
+    <el-divider content-position="left">沿街商户</el-divider>
+    <div id="elcard_Manage">
+      <el-card v-for="(item,index) in testCard" :key="index">
+        <div slot="header">
+          <span>角色名称</span>
+        </div>
+        <div>{{item.con}}</div>
+      </el-card>
+    </div>
+    <el-divider content-position="left">住宅区用户</el-divider>
+    <div id="elcard_Manage">
+      <el-card v-for="(item,index) in testCard" :key="index">
+        <div slot="header">
+          <span>角色名称</span>
+        </div>
+        <div>{{item.con}}</div>
+      </el-card>
+    </div>
+    <el-divider content-position="left">回收车驾驶员</el-divider>
+    <div id="elcard_Manage">
+      <el-card v-for="(item,index) in testCard" :key="index">
+        <div slot="header">
+          <span>角色名称</span>
+        </div>
+        <div>{{item.con}}</div>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -9,16 +37,28 @@ export default {
   name: "son1Manage",
   data() {
     return {
-      list: []
+      list: [],
+      testCard: [
+        {
+          con: "1"
+        },
+        {
+          con: "2"
+        },
+        {
+          con: "3"
+        }
+      ]
     };
   },
   mounted() {
-      this.getAllClassify();
+    // this.getAllClassify();
   },
   methods: {
     getAllClassify() {
       this.axios
-        .get("http://" + this.$store.state.path + ":8080/getAllClassify")
+        // .get("http://" + this.$store.state.path + ":8080/getAllClassify")
+        .get("http://" + this.$store.state.path + ":8080/getGarbage")
         //getGarbage获取垃圾
         .then(res => {
           console.log(res.data);
@@ -29,31 +69,32 @@ export default {
           console.log(err);
         });
     },
-    postData(){
-         this.axios
-          .post(
-            "http://"+self.$store.state.path+":8080/sourcecontroller/selectsourcefromsourcedrugid",
-            qs.stringify(data),
-            {
-              headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-              }
+    postData() {
+      this.axios
+        .post(
+          "http://" +
+            self.$store.state.path +
+            ":8080/sourcecontroller/selectsourcefromsourcedrugid",
+          qs.stringify(data),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
             }
-          )
-          .then(res => {
-              //数据二次转格式
-            self.purchaseForm = JSON.parse(JSON.stringify(self.purchaseForm));
-            console.log(
-              this.$store.state.purchaseDrugInfo.data[0].sourcedrugspe
-            );
-          })
-          .catch(err => {
-            console.log(err);
-          });
+          }
+        )
+        .then(res => {
+          //数据二次转格式
+          self.purchaseForm = JSON.parse(JSON.stringify(self.purchaseForm));
+          console.log(this.$store.state.purchaseDrugInfo.data[0].sourcedrugspe);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/css/son1Manage.scss";
 </style>
