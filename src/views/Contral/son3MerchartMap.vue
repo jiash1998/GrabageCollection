3159984fe828847654ff84db9dc48b5821d338d2
jiash1998 <1459538273@qq.com>
2020-01-16 15:59:24 +0800

    20.01.20bubbleCSSModify

--- src/views/Contral/son3MerchartMap.vue
+++ src/views/Contral/son3MerchartMap.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-      <p>merchartsmap</p>
+  <div id="son3MerchartMap.vue">
+    <baidu-map class="bm-view" ak="faARwTpILZCsY9S5GUKe6LL2ILicSoDX" center="滁州"></baidu-map>
   </div>
 </template>
 
 <script>
 //引入charts
-let echarts = require("echarts/lib/echarts");
-require("echarts/lib/chart/pie");
-require("echarts/lib/component/tooltip");
-require("echarts/lib/component/title");
-require("echarts/lib/component/legend");
-require("echarts/lib/component/legendScroll");
-export default {};
+// let echarts = require("echarts/lib/echarts");
+// require("echarts/lib/chart/pie");
+// require("echarts/lib/component/tooltip");
+//引入百度地图
+import BaiduMap from "vue-baidu-map/components/map/Map.vue";
+export default {
+  name: "son3MerchartMap",
+  components: {
+    BaiduMap
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.bm-view {
+  width: 100%;
+  height: 300px;
+}
 </style>
--- src/views/custom.vue
+++ src/views/custom.vue
@@ -1,18 +1,47 @@
 <template>
-    <div id="custom">
-        <div id="custom_body"></div>
-        <public-food></public-food>
+  <div id="custom">
+    <div id="custom_body">
+      <div class="custom_introduce">
+        <div class="test_con">
+          <div class="content1">
+            <div id="test1"></div>
+            <div id="test2"></div>
+            <div id="con_span">
+              <span>定制服务</span>
+            </div>
+          </div>
+          <div class="content2">
+            <div id="test3"></div>
+            <div id="test4"></div>
+            <div id="con_span">
+              <span>价格实惠</span>
+            </div>
+          </div>
+          <div class="content3">
+            <div id="test5"></div>
+            <div id="test6"></div>
+            <div id="con_span">
+              <span>方便快捷</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="custom_fill1"></div>
+      <div class="custom_fill2"></div>
+      <div class="custom_main"></div>
     </div>
+    <public-food></public-food>
+  </div>
 </template>
 
 <script>
-import PublicFood from '../components/publicFood.vue';
-    export default {
-        name:'custom',
-        components:{
-            PublicFood
-        }
-    }
+import PublicFood from "../components/publicFood.vue";
+export default {
+  name: "custom",
+  components: {
+    PublicFood
+  }
+};
 </script>
 
 <style lang="scss" scoped>
