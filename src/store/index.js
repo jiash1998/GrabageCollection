b1155340f78a679ebb31ce9392f35bc8d0915952
jiash1998 <1459538273@qq.com>
2020-01-16 17:21:39 +0800

    20.01.16customModify

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    path: "118.31.12.146"
+    path: "118.31.12.146",
+    custom: true
   },
-  mutations: {},
+  mutations: {
+    customModify(state) {
+      return (state.custom = false);
+    }
+  },
   actions: {},
   modules: {}
 });
--- src/views/custom.vue
+++ src/views/custom.vue
@@ -2,6 +2,10 @@
   <div id="custom">
     <div id="custom_body">
       <div class="custom_introduce">
+        <div class="intro_div">
+          <span id="intro_span1">CHZU</span>
+          <span id="intro_span2">始终强调</span>
+        </div>
         <div class="test_con">
           <div class="content1">
             <div id="test1"></div>
@@ -14,7 +18,7 @@
             <div id="test3"></div>
             <div id="test4"></div>
             <div id="con_span">
-              <span>价格实惠</span>
+              <span>经济实惠</span>
             </div>
           </div>
           <div class="content3">
@@ -28,7 +32,50 @@
       </div>
       <div class="custom_fill1"></div>
       <div class="custom_fill2"></div>
-      <div class="custom_main"></div>
+      <div class="custom_main">
+        <!-- <div id="custom_btn" v-if="$store.state.custom">
+          <el-button type="success" plain round @click="customClick">开始定制</el-button>
+        </div>-->
+        <div id="custom_main_con">
+          <h3>商家定制</h3>
+          <el-form :model="custom" ref="custom">
+            <el-form-item label="店铺名称">
+                <el-input v-model="custom.name" placeholder="请输入店铺名称"></el-input>
+            </el-form-item>
+            <el-form-item label="经营范围">
+                <el-radio-group v-model="custom.type">
+                  <el-radio label="快餐店"></el-radio>
+                  <el-radio label="火锅店"></el-radio>
+                  <el-radio label="大排档"></el-radio>
+                  <el-radio label="快餐店"></el-radio>
+                  <el-radio label="烧烤店"></el-radio>
+                  <el-radio label="奶茶店"></el-radio>
+                  <el-radio label="理发店"></el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="店铺地址">
+                <el-input v-model="custom.address" placeholder="请输入店铺名称"></el-input>
+            </el-form-item>
+            <el-form-item label="店铺负责人">
+                <el-input v-model="custom.header" placeholder="请输入负责人姓名"></el-input>
+            </el-form-item>
+             <el-form-item label="性别">
+              <el-radio-group v-model="custom.sex">
+                <el-radio-button label="男"></el-radio-button>
+                <el-radio-button label="女"></el-radio-button>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="联系方式（手机）">
+                <el-input v-model="custom.phone" placeholder="请输入手机号"></el-input>
+            </el-form-item>
+            <el-form-item label="社会信用代码">
+                <el-input v-model="custom.socialCreditCode" placeholder="请输入店铺名称"></el-input>
+            </el-form-item>
+            <el-form-item label="上传营业执照"></el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <div class="custom_custom"></div>
     </div>
     <public-food></public-food>
   </div>
@@ -40,6 +87,24 @@ export default {
   name: "custom",
   components: {
     PublicFood
+  },
+  data() {
+    return {
+      custom: {
+          name:'',
+          type:'',
+          address:'',
+          header:'',
+          sex:'',
+          phone:'',
+          socialCreditCode:''
+      }
+    };
+  },
+  methods: {
+    customClick() {
+      this.$store.commit("customModify");
+    }
   }
 };
 </script>
