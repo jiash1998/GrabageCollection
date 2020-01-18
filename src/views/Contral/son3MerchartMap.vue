b1afe107d8f44ad8914d3f296ff220031de3b239
jiash1998 <1459538273@qq.com>
2020-01-18 17:22:48 +0800

    20.01..18MapModify

--- src/views/Contral/son3MerchartMap.vue
+++ src/views/Contral/son3MerchartMap.vue
@@ -1,7 +1,20 @@
 <template>
-  <div id="son3MerchartMap.vue">
-    <baidu-map class="bm-view" ak="faARwTpILZCsY9S5GUKe6LL2ILicSoDX" center="滁州市">
+  <div id="son3MerchartMap">
+    <!-- zoom必须设置 -->
+    <baidu-map
+      class="bm-view"
+      ak="faARwTpILZCsY9S5GUKe6LL2ILicSoDX"
+      :center="map.center"
+      :zoom="map.zoom"
+    >
       <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+      <bm-marker :position="map.merchartPoint1" :dragging="false" @click="infoWindowOpen">
+        <bm-info-window :show="map.show" @close="infoWindowClose" @open="infoWindowOpen">我爱北京天安门</bm-info-window>
+      </bm-marker>
+      <bm-marker :position="map.merchartPoint2" :dragging="false" @click="infoWindowOpen">
+        <bm-info-window :show="map.show" @close="infoWindowClose" @open="infoWindowOpen">我爱北京天安门</bm-info-window>
+      </bm-marker>
+      <!-- <bm-local-search :keyword="addressKeyword" :auto-viewport="true" style="display: none"></bm-local-search> -->
     </baidu-map>
   </div>
 </template>
@@ -9,11 +22,42 @@
 <script>
 //引入百度地图
 import BaiduMap from "vue-baidu-map/components/map/Map.vue";
-// import BmNavigation from 'vue-baidu-map/components/others'
+//比例尺
+import BmNavigation from "vue-baidu-map/components/controls/Navigation";
+import BmLocalSearch from "vue-baidu-map/components/search/LocalSearch.vue";
+import BmMarker from "vue-baidu-map/components/overlays/Marker";
+import BmInfoWindow from "vue-baidu-map/components/overlays/InfoWindow";
 export default {
   name: "son3MerchartMap",
   components: {
-    BaiduMap
+    BaiduMap,
+    BmNavigation,
+    BmLocalSearch,
+    BmMarker,
+    BmInfoWindow
+  },
+  data() {
+    return {
+      map: {
+        center: {
+          lng: 118.323509,
+          lat: 32.282115
+        },
+        zoom: 20,
+        show: false,
+        merchartPoint1: { lng: 118.323617, lat: 32.282342 },
+        merchartPoint2: { lng: 118.323757, lat: 32.282674 }
+      }
+    };
+  },
+  methods: {
+    infoWindowClose() {
+      this.map.show = false;
+    },
+    infoWindowOpen() {
+      this.map.show = true;
+      console.log(this.map.show);
+    }
   }
 };
 </script>
@@ -21,6 +65,7 @@ export default {
 <style lang="scss" scoped>
 .bm-view {
   width: 100%;
-  height: 300px;
+  height: 400px;
+  margin-left: 3%;
 }
 </style>
--- src/views/Contral/son6Notice.vue
+++ src/views/Contral/son6Notice.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="son6Notice">
     <h4>公告</h4>
-    <el-form :model="notice" ref="notice">
-      <el-form-item label="公告标题">
+    <el-form :model="notice" ref="notice" :rules="rules">
+      <el-form-item label="公告标题" prop="title">
         <el-input v-model="notice.title" placeholder="请输入公告标题"></el-input>
       </el-form-item>
-      <el-form-item label="公告内容">
+      <el-form-item label="公告内容" prop="content">
         <el-input type="textarea" v-model="notice.content" placeholder="请输入公告内容"></el-input>
       </el-form-item>
-      <el-form-item label="公告标签">
+      <el-form-item label="公告标签" >
         <el-tag
           :key="tag"
           v-for="tag in dynamicTags"
@@ -28,17 +28,17 @@
         <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
         <p id="tag_p">最多输入三个标签</p>
       </el-form-item>
-      <el-form-item label="公告形式">
-          <el-radio-group v-model="notice.type">
-            <el-radio label="普通通知"></el-radio>
-            <el-radio label="商户通知"></el-radio>
-            <el-radio label="用户通知"></el-radio>
-            <el-radio label="紧急通知"></el-radio>
-          </el-radio-group>
+      <el-form-item label="公告形式" prop="type">
+        <el-radio-group v-model="notice.type">
+          <el-radio label="普通通知"></el-radio>
+          <el-radio label="商户通知"></el-radio>
+          <el-radio label="用户通知"></el-radio>
+          <el-radio label="紧急通知"></el-radio>
+        </el-radio-group>
       </el-form-item>
       <el-form-item label>
-          <el-button type="success" plain>发布公告</el-button>
-          <el-button type="success">返回</el-button>
+        <el-button type="success" @click="submitForm('notice')">发布公告</el-button>
+        <el-button type="primary" plain>返回</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -48,39 +48,89 @@
 export default {
   name: "son6Notice",
   data() {
+    var validateTitle = (rule, value, callback) => {
+      if (!value) {
+        return new callback("请输入标题");
+      } else {
+        callback();
+      }
+    };
+    var validateContent = (rule, value, callback) => {
+      if (!value) {
+        return new callback("请输入内容");
+      } else {
+        callback();
+      }
+    };
+    var validateInputvalue = (rule, value, callback) => {
+      if (!value) {
+        return new callback("请输入标签");
+      } else {
+        callback();
+      }
+    };
+    var validateType = (rule, value, callback) => {
+      if (!value) {
+        return new callback("请选择通知类型");
+      } else {
+        callback();
+      }
+    };
     return {
+      rules: {
+        title: [{ validator: validateTitle, trigger: "change" }],
+        content: [{ validator: validateContent, trigger: "change" }],
+        inputValue: [{ validator: validateInputvalue, trigger: "change" }],
+        type: [{ validator: validateType, trigger: "change" }]
+      },
       notice: {
         title: "",
         content: "",
-        inputValue:'',
-        type:''
+        inputValue: "",
+        type: ""
       },
       dynamicTags: [],
-      inputVisible: false,
+      inputVisible: false
     };
   },
   methods: {
-      handleClose(tag) {
-        this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
-      },
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
 
-      showInput() {
-        this.inputVisible = true;
-        this.$nextTick(_ => {
-          this.$refs.saveTagInput.$refs.input.focus();
-        });
-      },
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
 
-      handleInputConfirm() {
-        let inputValue = this.notice.inputValue;
-        if (inputValue) {
-            //添加到展示数组中
-          this.dynamicTags.push(inputValue);
-        }
-        this.inputVisible = false;
-        this.notice.inputValue = "";
+    handleInputConfirm() {
+      let inputValue = this.notice.inputValue;
+      if (inputValue) {
+        //添加到展示数组中
+        this.dynamicTags.push(inputValue);
       }
-    
+      // console.log(this.dynamicTags);
+      this.inputVisible = false;
+      this.notice.inputValue = "";
+    },
+
+    submitForm(formname) {
+      this.$refs[formname].validate(val => {
+        if (val) {
+          var data = [];
+          data[0] = this.notice;
+          data[1] = this.dynamicTags;
+          console.log(data);
+        } else {
+          var data = [];
+          data[0] = this.notice;
+          data[1] = this.dynamicTags;
+          return false;
+        }
+      });
+    }
   }
 };
 </script>
