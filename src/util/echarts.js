3220988ddedf9bd28cff905fc1ac723be5e69356
jiash1998 <1459538273@qq.com>
2020-04-10 17:49:47 +0800

    200410EchartsSecond

--- src/util/echarts.js
+++ src/util/echarts.js
@@ -4,7 +4,11 @@ import echarts from "echarts/lib/echarts";
 // 再引入你需要使用的图表类型，标题，提示信息等
 import "echarts/lib/chart/pie";
 import "echarts/lib/chart/sankey";
+import "echarts/lib/chart/line";
+
 import "echarts/lib/component/legend";
 import "echarts/lib/component/title";
+import "echarts/lib/component/tooltip";
+
 
 export default echarts;
--- src/views/Contral/son8Echarts.vue
+++ src/views/Contral/son8Echarts.vue
@@ -8,6 +8,9 @@
     </div>
     <div class="secondItem">
       <p>已定制店铺分析</p>
+      <el-card shadow="hover">
+        <div id="mycharts2" ref="mycharts2"></div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -19,10 +22,11 @@ export default {
   name: "son8Echarts",
   mounted() {
     this.draw();
+    this.draw2();
   },
   methods: {
     draw() {
-      mycharts = echarts.init(this.$refs.mycharts);
+      var mycharts = echarts.init(this.$refs.mycharts);
       mycharts.setOption({
         series: {
           type: "sankey",
@@ -65,6 +69,71 @@ export default {
           ]
         }
       });
+    },
+
+    draw2() {
+      var myCharts2 = echarts.init(this.$refs.mycharts2);
+
+      var option = {
+        legend: {},
+        tooltip: {
+          trigger: "axis",
+          showContent: false
+        },
+        dataset: {
+          source: [
+            ["product", "2012", "2013", "2014", "2015", "2016", "2017"],
+            ["Matcha Latte", 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
+            ["Milk Tea", 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
+            ["Cheese Cocoa", 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
+            ["Walnut Brownie", 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
+          ]
+        },
+        xAxis: { type: "category" },
+        yAxis: { gridIndex: 0 },
+        grid: { top: "60%" },
+        series: [
+          { type: "line", smooth: true, seriesLayoutBy: "row" },
+          { type: "line", smooth: true, seriesLayoutBy: "row" },
+          { type: "line", smooth: true, seriesLayoutBy: "row" },
+          { type: "line", smooth: true, seriesLayoutBy: "row" },
+          {
+            type: "pie",
+            id: "pie",
+            radius: "25%",
+            center: ["50%", "30%"],
+            label: {
+              formatter: "{b}: {@2012} ({d}%)"
+            },
+            encode: {
+              itemName: "product",
+              value: "2012",
+              tooltip: "2012"
+            }
+          }
+        ]
+      };
+
+      myCharts2.on("updateAxisPointer", function(event) {
+        var xAxisInfo = event.axesInfo[0];
+        if (xAxisInfo) {
+          var dimension = xAxisInfo.value + 1;
+          myCharts2.setOption({
+            series: {
+              id: "pie",
+              label: {
+                formatter: "{b}: {@[" + dimension + "]} ({d}%)"
+              },
+              encode: {
+                value: dimension,
+                tooltip: dimension
+              }
+            }
+          });
+        }
+      });
+
+      myCharts2.setOption(option);
     }
   }
 };
